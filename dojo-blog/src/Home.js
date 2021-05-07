cbbcc1ae333ab2df8077f0deb32fe04d99b22d9e
oaa97181 <oscar@nivelate.mx>
2021-05-06 20:40:59 -0500

    add some click events :D

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Homepage Yayyyyyy</h2>
-        </div>
-     );
-}
- 
-export default Home;
+
+    const handleClick = (e) => {
+      console.log('hello ninjas', e);
+    }
+  
+    const handleClickAgain = (name, e) => {
+      console.log('hello ' + name, e.target);
+    }
+  
+    return (
+      <div className="home">
+        <h2>Homepage</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+      </div>
+    );
+  }
+  
+  export default Home; 

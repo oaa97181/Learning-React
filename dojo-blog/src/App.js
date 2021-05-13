6785ea356129343693665b3e189c920f5a4e8cc9
oaa97181 <oscar@nivelate.mx>
2021-05-12 19:47:23 -0500

    remove unused consts

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,19 +2,14 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome, stranger 🕵🏻‍♀️'
-  const likes = 123456789
-  // const person = { name: 'juana', age: 22 };
-  const link = 'http://www.google.com';
-
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content">
-        <Home />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar></Navbar>
+            <div className="content">
+                <Home />
+            </div>
+        </div>
+    );
 }
 
 export default App;

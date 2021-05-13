0f710a5b300d8b08e3423e8abcafaaa36251246f
oaa97181 <oscar@nivelate.mx>
2021-05-13 13:53:44 -0500

    make a custom hook and make it reusable!!!

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,32 +1,11 @@
 // to run json-server do:
 // npx json-server --watch data/db.json --port 8000
 
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        fetch(' http://localhost:8000/blogss')
-            .then(res => {
-                if (!res.ok){
-                    throw Error('could not fecth data for that source, sorry ☹')
-                }
-                return res.json()
-            })
-            .then(data=>{
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(err => {
-                setError(err.message)
-                setIsPending(false)
-            })
-    }, []);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="home">
@@ -35,6 +14,6 @@ const Home = () => {
             {blogs && <BlogList blogs={blogs} title="All blogs listed down below 👇🏻"/>}
         </div>
     );
-};
+}
 
 export default Home;

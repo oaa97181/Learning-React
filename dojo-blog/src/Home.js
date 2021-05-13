4ec152e34366c1ef9e0a89da3e87b77ad990b5a5
oaa97181 <oscar@nivelate.mx>
2021-05-12 20:04:57 -0500

    fetch data w/ useEffect() & some conditional templating!

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,27 +1,25 @@
+// to run json-server do:
+// npx json-server --watch data/db.json --port 8000
+
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: '1st Article', body: 'lorem ipsum...', author: 'oaa', id: 1 },
-        { title: '2nd Article!', body: 'lorem ipsum...', author: 'dani', id: 2 },
-        { title: '3rd Article', body: 'lorem ipsum...', author: 'adrian', id: 3 },
-        { title: 'Last Article', body: 'lorem ipsum...', author: 'oaa', id: 99 },
-    ])
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    const [blogs, setBlogs] = useState(null)
 
     useEffect(() => {
-        console.log('in!')
+        fetch(' http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data=>{
+                setBlogs(data)
+            })
     }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs listed down below 👇🏻" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'oaa' )} title="All oaa's blogs listed down below 👇🏻"/>
+            {blogs && <BlogList blogs={blogs} title="All blogs listed down below 👇🏻"/>}
         </div>
     );
 }

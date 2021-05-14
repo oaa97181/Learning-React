c1ee8b4bd0500c10eebcba86e53cf793c4863ee2
oaa97181 <oscar@nivelate.mx>
2021-05-13 19:15:02 -0500

    exact match routes on '/'

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,16 +1,20 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from "./Create";
 import { BrowserRouter as  Router, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Navbar></Navbar>
+                <Navbar />
                 <div className="content">
                     <Switch>
-                        <Route path='/home'>
+                        <Route exact path='/'>
                             <Home />
+                        </Route>
+                        <Route path='/create'>
+                            <Create />
                         </Route>
                     </Switch>
                 </div>
